68ca2dd395a4f6ff5ed0631dc21c6eff6367ecf6
vishalkhandal <vishalkhandal96@gmail.com>
2025-05-17 17:09:21 +0530

    add .env file

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -3,7 +3,7 @@ import { Client, Storage, ID } from 'appwrite';
 const client = new Client();
 client
     .setEndpoint('https://fra.cloud.appwrite.io/v1')
-    .setProject(import.meta.env.PROJECT_ID);
+    .setProject(import.meta.env.VITE_PROJECT_ID);
 
 const storage = new Storage(client)
 
